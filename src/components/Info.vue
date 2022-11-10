110d2a53b67e54c04e9416ee06bbeb6c4c64b7d2
euliberato <ruanlib.2003@gmail.com>
2022-11-10 11:29:59 -0300

    Aprendendo a renderizar vetores e listas com objetos

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,11 +1,15 @@
 <template>
   <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
   <p v-else>Estou em busca de novas oportunidades!</p>
-  <p>Utilizo as seguintes tecnologias:</p>
+  <p>Utilizo as seguintes tecnologias (Back-end):</p>
   <ul>
-    <li>JavaScript</li>
-    <li>PHP</li>
-    <li>Python</li>
+    <li v-for="(techonology, index) in backend_technologies" v-bind:key="index">{{ techonology }}</li>
+  </ul>
+  <p>Utilizo as seguintes tecnologias (Front-end):</p>
+  <ul>
+    <li v-for="technology in frontend_technologies" :key="technology.id">
+      {{ technology.language }}
+    </li>
   </ul>
   <div>
     <button @click="showEmail">{{ textoBotao }}</button>
@@ -26,7 +30,13 @@ export default {
             mostrar_email: false,
             email: "ruanlib@gmail.com",
             meu_link: "https://google.com",
-            textoBotao: "Mostrar e-mail"
+            textoBotao: "Mostrar e-mail",
+            backend_technologies: ["JavaScript", "PHP", "Python"],
+            frontend_technologies: [
+              {id: 1, language: 'HTML'},
+              {id: 2, language: 'CSS'},
+              {id: 1, language: 'VueJS'}
+            ]
         };
     },
     components: { Picture },
